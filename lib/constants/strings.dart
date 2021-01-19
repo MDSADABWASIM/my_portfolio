f9db5d8a4e1a48bd0d3b87371804d86e4ef4f6eb
sadabwasim <sadab.wasim@inndata.in>
2021-01-19 11:36:19 +0530

    Initial commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,20 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = ' Me';
+  static const String portfoli = 'Md. Sadab';
+  static const String o = ' Wasim';
+  static const String headline = 'Data Scientist';
+  static const String summary = 'I\'m  a Software engineer with 3 years of experience and quite passionate about Data Science and Researches that is going on in the AI field.';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved = 'Md Sadab Wasim, ALL RIGHTS RESERVED.';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'About';
+  static const String menu_contact = 'Contact';
+
+}
